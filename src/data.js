1ea370dd28abdeda503d55857bd1f99c0c542a68
Aarif <adebayoabdulazeez195@gmail.com>
2024-11-08 23:33:29 -0800

    chore: creation of html and css file

---
+++ src/data.js
@@ -0,0 +1,32 @@
+let shopItemData = [{
+  id: "fhhfhf",
+  name: "Casual Shirt",
+  price: 45,
+  desc: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
+  img: "imgaes/shirt1.jpg"
+},
+
+{
+  id: "fffjjffjjf",
+  name: "Office Shirt",
+  price: 100,
+  desc: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
+  img: "imgaes/shirt2.jpg"
+},
+
+{
+  id: "hddjfjfj",
+  name: "T Shirt",
+  price: 25,
+  desc: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
+  img: "imgaes/shirt4.jpg"
+},
+
+{
+  id: "fggg",
+  name: "Men Suit",
+  price: 300,
+  desc: "Lorem, ipsum dolor sit amet consectetur adipisicing.",
+  img: "imgaes/shirt2.jpg"
+}
+]
